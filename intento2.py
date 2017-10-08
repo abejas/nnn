f59650b1fbe8f5fbcc3b527d426c7551c7bfe643
abejas <q1243889@mvrht.net>
2017-10-08 22:25:55 +0000

    ahhhh

---
+++ intento2.py
@@ -0,0 +1,11 @@
+#/home/pi/Documents/abejas2
+#intento2.py
+#Esto es un comentario
+#Importamos la librería time
+import time
+print("Hola Mundo")
+print("...")
+time.sleep(5)
+print("Pasados 5 segundos")
+print("Hasta luego Mundo")
+
